73b70fcc07958b337401762227588207fc899285
uzair-k <67186029+uzair-k@users.noreply.github.com>
2020-08-13 01:36:36 +0500

    Update settings.py

--- Order Management/env/pro/pro/settings.py
+++ Order Management/env/pro/pro/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'peterpiper1212154@gmail.com'
-EMAIL_HOST_PASSWORD = 'Wh@@@@tUp'
+EMAIL_HOST_USER = 'Your Username'
+EMAIL_HOST_PASSWORD = 'Your Password'
