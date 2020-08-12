cba9f8724c79e3084b4f479a7148839b2a0201b8
uzair-k <67186029+uzair-k@users.noreply.github.com>
2020-08-13 01:37:23 +0500

    Update settings.py

--- Order Management/env/pro/pro/settings.py
+++ Order Management/env/pro/pro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@3^2rbtwdhx%6jxe=46)ns!*8h#2u09q6dv!hom%@a71xbi*_('
+SECRET_KEY = 'Your Django Secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
